016a8672b25202e56ec7fbdd2132c4594177f0c1
leegeon <dlrjs1509@naver.com>
2022-05-30 17:57:27 +0900

    0530

--- flat-interior/js/main.js
+++ flat-interior/js/main.js
@@ -4,6 +4,10 @@ $(function(){
     var cnt = 0;
     var flag = false;
     var btn = $('.sidebar .image');
+    var bg = $('.Modal_menubar_bg');
+    var md = $('.Modal_menubar');
+    var cls = $('.Modal_menubar .menuHdr a');
+    var menu = $('.menu span');
     for(var i = 0; i < $('.cntWrap .content').length; i++){
         num1[i] = $('.cntWrap .content').eq(i).offset().top;
     }
@@ -13,11 +17,15 @@ $(function(){
             if(x < 0){
                 cnt++;
                 flag = true;
-                if(cnt>=4){cnt=4;}
+                if(cnt>=3){cnt=3;}
+                btn.removeClass('on');
+                btn.eq(cnt).addClass('on');
             }else{
                 cnt--;
                 flag = true;
                 if(cnt<=0){cnt=0}
+                btn.removeClass('on');
+                btn.eq(cnt).addClass('on');
             }
             $('html').animate({
                 scrollTop:num1[cnt]
@@ -26,12 +34,23 @@ $(function(){
             });
         }
     });
+    menu.click(function(){
+        bg.addClass('on');
+        md.addClass('on');
+    });
+    cls.click(function(){
+        bg.removeClass('on');
+        md.removeClass('on');
+    });
     btn.click(function(){
         var ind = $(this).index();
-        btn.removeClass('on')
+        cnt = ind;
+        btn.removeClass('on');
         btn.eq(ind).addClass('on');
         $('html').animate({
             scrollTop : num1[ind]
         });
     });
+    btn.eq(0).addClass('on');
 });
+history.scrollRestoration = "manual";
